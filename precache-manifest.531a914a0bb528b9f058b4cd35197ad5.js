d560c7a8c8b2a9bdf35c670ac8d9d4f7e4149d6d
Adam Chew <adamwth@hotmail.com>
2020-09-01 11:09:36 +0800

    Updates

---
+++ precache-manifest.531a914a0bb528b9f058b4cd35197ad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f15c2161e09042863fa9a9cc26f509f1",
+    "url": "/gif-search-engine/index.html"
+  },
+  {
+    "revision": "4dc929fa6fa214f36d27",
+    "url": "/gif-search-engine/static/js/2.10571c5e.chunk.js"
+  },
+  {
+    "revision": "f129a25a95b5779003d2444137eb2900",
+    "url": "/gif-search-engine/static/js/2.10571c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aea4c149ccf8e9f5273d",
+    "url": "/gif-search-engine/static/js/main.003d2726.chunk.js"
+  },
+  {
+    "revision": "97e7a2b0f9c5c2c59e9f",
+    "url": "/gif-search-engine/static/js/runtime-main.ccd7a148.js"
+  }
+]);
